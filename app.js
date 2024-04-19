4518eae67c0948a2be7725d42ae0bf9f85922574
oli <feketeoliver2006@gmail.com>
2024-04-19 14:16:48 +0200

    basics

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+    Szavak listája
+*/
+const words = [
+    "Dog", "Cat", "Sun", "Run", "Pen",
+    "Bat", "Car", "Bus", "Cap", "Fan",
+    "Cow", "Box", "Cup", "Key", "Egg",
+    "Lip", "Arm", "Leg", "Arm", "Gum",
+    "Jam", "Log", "Map", "Nap", "Oak",
+    "Pig", "Rug", "Toy", "Wig", "Ant",
+    "Bow", "Cow", "Dry", "Elf", "Fog",
+    "God", "Hen", "Ink", "Jug", "Kit",
+    "Lip", "Man", "Net", "Owl", "Cop",
+    "Rod", "Sky", "Toy", "Van", "Zip"
+];
