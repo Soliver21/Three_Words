911d502adf5a0f800d93500ea862100dcf709148
oli <feketeoliver2006@gmail.com>
2024-04-24 17:34:33 +0200

    app

--- app.js
+++ app.js
@@ -1,34 +1,143 @@
-/*
-    Szavak listája
-*/
-const words = [
-    "Dog", "Cat", "Sun", "Run", "Pen",
-    "Bat", "Car", "Bus", "Cap", "Fan",
-    "Cow", "Box", "Cup", "Key", "Egg",
-    "Lip", "Arm", "Leg", "Arm", "Gum",
-    "Jam", "Log", "Map", "Nap", "Oak",
-    "Pig", "Rug", "Toy", "Wig", "Ant",
-    "Bow", "Cow", "Dry", "Elf", "Fog",
-    "God", "Hen", "Ink", "Jug", "Kit",
-    "Lip", "Man", "Net", "Owl", "Cop",
-    "Rod", "Sky", "Toy", "Van", "Zip"
+let keys = {
+  'q': '', 'w': '', 'e': '', 'r': '', 't': '', 'y': '', 'u': '', 'i': '', 'o': '', 'p': '', 'break': '',
+  'a': '', 's': '', 'd': '', 'f': '', 'g': '', 'h': '', 'j': '', 'k': '', 'l': '', 'break2': '',
+  'enter': '', 'z': '', 'x': '', 'c': '', 'v': '', 'b': '', 'n': '', 'm': '', '⌫': ''
+};
+
+let guesses = [];
+let currentGuess = [];
+
+const wordList = [
+  "Dog", "Cat", "Sun", "Run", "Pen",
+  "Bat", "Car", "Bus", "Cap", "Fan",
+  "Cow", "Box", "Cup", "Key", "Egg",
+  "Lip", "Arm", "Leg", "Arm", "Gum",
+  "Jam", "Log", "Map", "Nap", "Oak",
+  "Pig", "Rug", "Toy", "Wig", "Ant",
+  "Bow", "Cow", "Dry", "Elf", "Fog",
+  "God", "Hen", "Ink", "Jug", "Kit",
+  "Lip", "Man", "Net", "Owl", "Cop",
+  "Rod", "Sky", "Toy", "Van", "Zip"
 ];
+const SecretWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-document.addEventListener("DOMContentLoaded", () => {
-    let container = document.querySelector('.container');
+const NumberOfGuesses = 6;
+const Correct = 'correct';
+const Found = 'found';
+const Wrong = 'wrong';
 
-    for (let i = 0; i < 6; i++) {
-        for (let j = 0; j < 3; j++) {
-            let input = document.createElement('input');
-            input.maxLength = "1";
-            input.type = 'text';
-            input.classList.add('guess-input');
-            container.appendChild(input);
-        }
-        container.appendChild(document.createElement('br'));   
+function initialize() {
+  let guessGrid = document.getElementById("guessGrid");
+  for (let i = 0; i < NumberOfGuesses; i++) {
+    for (let j = 0; j < SecretWord.length; j++) {
+      guessGrid.innerHTML += `<div id="${i}${j}" class="key-guess"></div>`;
     }
-    container.innerHTML += `<button type="button" id="submitButton">Submit</button>`;
-});
+    guessGrid.innerHTML += '<br/>';
+  }
+
+  let keyboard = document.getElementById("keyboard");
+  Object.keys(keys).forEach((key) => {
+    if (key.includes('break')) {
+      keyboard.innerHTML += '<br/>';
+    } else {
+      keyboard.innerHTML += `<button id="${key}" class="key" onclick="keyClick('${key}')">` + key + '</button>';
+    }
+  });
+}
+initialize();
+
+function keyClick(key) {
+  switch (key) {
+    case '⌫':
+      backspace();
+      break;
+    case 'enter':
+      enter();
+      break;
+    default:
+      if (currentGuess.length < SecretWord.length
+        && guesses.length < NumberOfGuesses) {
+        currentGuess.push({ key: key, result: '' });
+        updateCurrentGuess();
+      }
+  }
+}
+
+function backspace() {
+  if (currentGuess.length > 0) {
+    currentGuess.pop();
+  }
+  updateCurrentGuess();
+}
+
+function enter() {
+  if (currentGuess.length < SecretWord.length || guesses.length >= NumberOfGuesses) {
+    if (guesses.length >= NumberOfGuesses) {
+      window.alert("You lost! The word was: " + SecretWord);
+    }
+    return;
+  }
+  let isCorrect = true;
+  for (let i = 0; i < SecretWord.length; i++) {
+    if (SecretWord.charAt(i) !== currentGuess[i].key) {
+      isCorrect = false;
+      break;
+    }
+  }
+  currentGuess.forEach((keyGuess, index) => {
+    if (isCorrect) {
+      keyGuess.result = Correct;
+    } else if (SecretWord.includes(keyGuess.key)) {
+      if (SecretWord.indexOf(keyGuess.key) === index) { // Check if the letter is in the correct place
+        keyGuess.result = Correct;
+      } else {
+        keyGuess.result = Found;
+      }
+    } else {
+      keyGuess.result = Wrong;
+    }
+
+    if (keys[keyGuess.key] !== Correct) {
+      keys[keyGuess.key] = keyGuess.result;
+    }
+  });
+  updateCurrentGuess(true);
+  if (isCorrect) {
+    window.alert("Congratulations! You won!");
+  }
+  guesses.push(currentGuess);
+  currentGuess = [];
+}
+
+function updateKeyboard() {
+  for (const key in keys) {
+    if (keys[key] !== '') {
+      let keyElement = document.getElementById(`${key}`);
+      keyElement.className = '';
+      keyElement.classList.add(keys[key]);
+      keyElement.classList.add('key');
+    }
+  }
+}
+
+function updateCurrentGuess(guessed = false) {
+  let index = guesses.length;
+  for (let i = 0; i < SecretWord.length; i++) {
+    let guessGrid = document.getElementById(`${index}${i}`);
+    if (currentGuess[i]) {
+      guessGrid.innerHTML = currentGuess[i].key;
+    } else {
+      guessGrid.innerHTML = '';
+    }
+    if (guessed) {
+      guessGrid.classList.add(currentGuess[i].result);
+    }
+  }
+  if (guessed) {
+    updateKeyboard();
+  }
+}
+
 
 const icon = document.querySelector('.ico img');
 const penis = document.querySelector(".ico p");
